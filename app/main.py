f63f0e9a9f7b3e4a09b031d6952ddfa8ff7b76de
aleksandrazt <s.tsarkova2013@yandex.ru>
2025-07-29 10:15:07 +0300

    пара правок на свежую голову

--- app/main.py
+++ app/main.py
@@ -2,22 +2,13 @@
 Точка входа
 """
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
 
-from . import crud, models, schemas, tron
-from .database import DatabaseManager
-
-db_manager = DatabaseManager()
-
-# вообще было бы лучше асинхронно все и оставила потенциально методы для того
-# но так как в задании не указано, что нужно использовать асинхронный подход,
-# то оставим синхронный подход для простоты и понятности + облегчение миграции
-# а так лучше ассинхронно через алембик
-models.Base.metadata.create_all(bind=db_manager.sync_engine)
+from .route import router
 
 app = FastAPI()
+app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -26,32 +17,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.post("/wallet-info/", response_model=schemas.WalletInfoResponse)
-def get_wallet_info(
-    wallet_request: schemas.WalletInfoRequest,
-    db: Session = Depends(db_manager.get_session),
-):
-    """
-    Получить данные по кошельку
-    """
-    wallet_info = tron.get_wallet_info_sync(wallet_request.wallet_address)
-    if not wallet_info:
-        raise HTTPException(status_code=404, detail="Wallet not found")
-
-    crud.create_wallet_query(db, schemas.WalletInfoResponse(**wallet_info))
-
-    return wallet_info
-
-
-@app.get("/query-history/", response_model=schemas.PaginatedQueryHistory)
-def get_query_history(
-    page: int = 1, limit: int = 10, db: Session = Depends(db_manager.get_session)
-):
-    """
-    Получить историю по кошельку
-    """
-    skip = (page - 1) * limit
-    result = crud.get_query_history(db, skip=skip, limit=limit)
-    return schemas.PaginatedQueryHistory(**result)
---
+++ app/route.py
@@ -0,0 +1,46 @@
+"""
+Маршрутизация запросов
+"""
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas, tron
+from .database import DatabaseManager
+
+router = APIRouter()
+db_manager = DatabaseManager()
+# вообще было бы лучше асинхронно все и оставила потенциально методы для того
+# но так как в задании не указано, что нужно использовать асинхронный подход,
+# то оставим синхронный подход для простоты и понятности + облегчение миграции
+# а так лучше ассинхронно через алембик
+models.Base.metadata.create_all(bind=db_manager.sync_engine)
+
+
+@router.post("/wallet-info/", response_model=schemas.WalletInfoResponse)
+def get_wallet_info(
+    wallet_request: schemas.WalletInfoRequest,
+    db: Session = Depends(db_manager.get_session),
+):
+    """
+    Получить данные по кошельку
+    """
+    wallet_info = tron.get_wallet_info_sync(wallet_request.wallet_address)
+    if not wallet_info:
+        raise HTTPException(status_code=404, detail="Wallet not found")
+
+    crud.create_wallet_query(db, schemas.WalletInfoResponse(**wallet_info))
+
+    return wallet_info
+
+
+@router.get("/query-history/", response_model=schemas.PaginatedQueryHistory)
+def get_query_history(
+    page: int = 1, limit: int = 10, db: Session = Depends(db_manager.get_session)
+):
+    """
+    Получить историю по кошельку
+    """
+    skip = (page - 1) * limit
+    result = crud.get_query_history(db, skip=skip, limit=limit)
+    return schemas.PaginatedQueryHistory(**result)
